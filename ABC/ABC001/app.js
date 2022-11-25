fdf7ff184b4de0e8f5da0f8c4989f20b26427187
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2022-11-25 21:48:56 +0900

    いっちい修正版

--- ABC/ABC001/app.js
+++ ABC/ABC001/app.js
@@ -4,3 +4,14 @@ function main(input) {
 }
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+//いっちい修正版
+/*function Main(input) {  
+	input = input.split("\n");
+
+	//文字列から10進数に変換するときはparseIntを使います
+	//出力
+	console.log(parseInt(input[0], 10) - parseInt(input[1], 10));
+}
+
+Main(require("fs").readFileSync("/dev/stdin", "utf8")); */

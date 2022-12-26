5588bd02e067cdeadf97f11981520d3843493581
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2022-12-27 00:35:28 +0900

    ABC18完了

---
+++ ABC/ABC18/app.js
@@ -0,0 +1,26 @@
+var lines=[];
+var reader = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+reader.on('line', function (line) {
+	lines.push(line);
+});
+
+reader.on('close', function () {
+	const A = parseInt(lines[0]);
+	const B = parseInt(lines[1]);
+	const C = parseInt(lines[2]);
+	const max = Math.max(A, B, C);
+	const min = Math.min(A, B, C);
+	[A, B, C].forEach(ele => {
+		if (ele === max) {
+			console.log('1');
+		} else if (ele === min) {
+			console.log('3');
+		} else {
+			console.log('2');
+		}
+	});
+});
